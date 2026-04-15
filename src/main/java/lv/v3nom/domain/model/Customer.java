64a31b6695b96ad55d27d918bd77ceb9a084a345
lxcalbxy <ycxsbeats@yandex.ru>
2026-04-15 03:00:19 +0300

    feat: add Customer entity business behaviour

--- src/main/java/lv/v3nom/domain/model/Customer.java
+++ src/main/java/lv/v3nom/domain/model/Customer.java
@@ -4,41 +4,96 @@ import lv.v3nom.domain.value.CustomerId;
 import lv.v3nom.domain.value.EmailAddress;
 import lv.v3nom.domain.value.PhoneNumber;
 import lv.v3nom.infrastructure.util.IdGenerator;
+import lv.v3nom.infrastructure.util.impl.SystemDateTimeProvider;
 
 import java.time.LocalDateTime;
 
 public class Customer {
-    private CustomerId id = CustomerId.generate();
+    private final CustomerId id;
     private String name;
     private EmailAddress email;
     private PhoneNumber phoneNumber;
-    private LocalDateTime createdAt;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    public CustomerId getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
+    private Customer(CustomerId id,
+                     String name,
+                     EmailAddress email,
+                     PhoneNumber phoneNumber,
+                     LocalDateTime createdAt) {
+        this.id = id;
         this.name = name;
-    }
-    public EmailAddress getEmail() {
-        return email;
-    }
-    public void setEmail(EmailAddress email) {
         this.email = email;
-    }
-    public PhoneNumber getPhoneNumber() {
-        return phoneNumber;
-    }
-    public void setPhoneNumber(PhoneNumber phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
+
+    public static Customer register(String name,
+                                    String email,
+                                    String phone) {
+        return new Customer(
+                CustomerId.generate(),
+                name.trim(),
+                EmailAddress.of(email),
+                PhoneNumber.of(phone),
+                new SystemDateTimeProvider().now());
+    }
+    public static Customer reconstitute(CustomerId id,
+                                       String name,
+                                       EmailAddress email,
+                                       PhoneNumber phoneNumber,
+                                       LocalDateTime createdAt,
+                                       LocalDateTime updatedAt) {
+        Customer customer = new Customer(
+                id, name, email, phoneNumber, createdAt);
+        customer.updatedAt = updatedAt;
+        return customer;
+    }
+    // TODO
+    // - void changeName
+    public void changeName(String newName) {
+        if (newName.isBlank() || newName == null) {
+            throw new IllegalArgumentException(
+                    "New customer name cannot be blank or null");
+        }
+        if (newName.length() < 3 || newName.length() > 45) {
+            throw new IllegalArgumentException(
+                    "Exceptional name length: " + newName.length()
+                            + String.format(". Allowed name length from %s to %s characters.", 3, 45));
+        }
+        this.name = newName;
+    }
+    // - void changeEmail
+    public void changeEmail(EmailAddress newEmail) {
+        if (newEmail == null) {
+            throw new IllegalArgumentException("New email cannot be null");
+        }
+        if (newEmail.equals(this.email)) {
+            throw new IllegalArgumentException("New email cannot be same");
+        }
+        this.email = newEmail;
+    }
+    // - void changePhoneNumber
+    public void changePhoneNumber(PhoneNumber newPhoneNumber) {
+        if (newPhoneNumber == null) {
+            throw new IllegalArgumentException("New phone number cannot be null");
+        }
+        if (newPhoneNumber.equals(this.phoneNumber)) {
+            throw new IllegalArgumentException("New phone number cannot be same");
+        }
+        this.phoneNumber = newPhoneNumber;
+    }
+
+    // query
+    // - bool canOpenAccount
+    public boolean canOpenAccount() {
+
+    }
+
+    public CustomerId getId() { return id; }
+    public String getName() { return name; }
+    public EmailAddress getEmail() { return email; }
+    public PhoneNumber getPhoneNumber() { return phoneNumber; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
 }
