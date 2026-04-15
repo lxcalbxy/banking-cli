500941f4f8329111d24bf1bfafb03473a2d22fb8
lxcalbxy <ycxsbeats@yandex.ru>
2026-04-15 03:06:31 +0300

    feat: add LocalDateTime.of() functionality

--- src/main/java/lv/v3nom/infrastructure/util/DateTimeProvider.java
+++ src/main/java/lv/v3nom/infrastructure/util/DateTimeProvider.java
@@ -3,5 +3,6 @@ package lv.v3nom.infrastructure.util;
 import java.time.LocalDateTime;
 
 public interface DateTimeProvider {
+    LocalDateTime of(int year, int month, int dayOfMonth, int hour, int minute, int second);
     LocalDateTime now();
 }
--- src/main/java/lv/v3nom/infrastructure/util/impl/SystemDateTimeProvider.java
+++ src/main/java/lv/v3nom/infrastructure/util/impl/SystemDateTimeProvider.java
@@ -6,6 +6,11 @@ import java.time.LocalDateTime;
 
 public class SystemDateTimeProvider implements DateTimeProvider {
     @Override
+    public LocalDateTime of(int year, int month, int dayOfMonth, int hour, int minute, int second) {
+        return LocalDateTime.of(year, month, dayOfMonth, hour, minute, second);
+    }
+
+    @Override
     public LocalDateTime now() {
         return LocalDateTime.now();
     }
