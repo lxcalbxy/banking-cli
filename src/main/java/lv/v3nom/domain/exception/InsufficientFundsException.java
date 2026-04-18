eab0f028ee572b19e964410d47826ece1a577d26
lxcalbxy <ycxsbeats@yandex.ru>
2026-04-19 01:08:19 +0300

    feat: add InsufficientFundsException

--- src/main/java/lv/v3nom/domain/exception/InsufficientFundsException.java
+++ src/main/java/lv/v3nom/domain/exception/InsufficientFundsException.java
@@ -1,4 +1,18 @@
 package lv.v3nom.domain.exception;
 
-public class InsufficientFundsException {
+import lv.v3nom.domain.value.Money;
+
+public class InsufficientFundsException extends RuntimeException {
+    private final Money balance;
+    private final Money attemptedAmount;
+
+    public InsufficientFundsException(Money balance, Money attemptedAmount) {
+        super(String.format("Value %s is too large. Balance: ", attemptedAmount, balance));
+
+        this.balance = balance;
+        this.attemptedAmount = attemptedAmount;
+    }
+
+    public Money getBalance() { return balance; }
+    public Money getAttemptedAmount() { return attemptedAmount; }
 }
