24259501d1c83e0261968fc68809c1171753c190
lxcalbxy <ycxsbeats@yandex.ru>
2026-04-19 20:59:08 +0300

    refactor: mend AuthenticationException

--- src/main/java/lv/v3nom/domain/exception/AuthenticationException.java
+++ src/main/java/lv/v3nom/domain/exception/AuthenticationException.java
@@ -1,15 +1,47 @@
 package lv.v3nom.domain.exception;
 
+import lv.v3nom.domain.value.AccountId;
 import lv.v3nom.domain.value.CustomerId;
+import lv.v3nom.domain.value.Token;
+import lv.v3nom.domain.value.TokenStatus;
 
 public class AuthenticationException extends RuntimeException {
+    private final AccountId accountId;
     private final CustomerId customerId;
+    private final Token token;
+    private final TokenStatus tokenStatus;
 
+    public AuthenticationException() {
+        super("Authentication failed");
+        this.accountId = null;
+        this.customerId = null;
+        this.token = null;
+        this.tokenStatus = null;
+    }
+    public AuthenticationException(AccountId accountId) {
+        super(String.format("Authentication failed. Account ID=[%s]", accountId.getValue()));
+        this.accountId = accountId;
+        this.customerId = null;
+        this.token = null;
+        this.tokenStatus = null;
+    }
     public AuthenticationException(CustomerId customerId) {
-        super(String.format("Authentication error for user %s", customerId.toString()));
-
+        super(String.format("Authentication failed. Customer ID=[%s]", customerId.getValue()));
+        this.accountId = null;
         this.customerId = customerId;
+        this.token = null;
+        this.tokenStatus = null;
+    }
+    public AuthenticationException(Token token) {
+        super(String.format("Authentication failed. Customer=[%s], Token=[%s]; Expiry=[%s]", token.getCustomerId(), token.getValue(), token.getExpiry()));
+        this.accountId = null;
+        this.customerId = null;
+        this.token = token;
+        this.tokenStatus = null;
     }
 
-    public CustomerId getCustomerId() { return this.customerId; }
+    public AccountId getAccountId() { return accountId; }
+    public CustomerId getCustomerId() { return customerId; }
+    public Token getToken() { return token; }
+    public TokenStatus getTokenStatus() { return tokenStatus; }
 }
