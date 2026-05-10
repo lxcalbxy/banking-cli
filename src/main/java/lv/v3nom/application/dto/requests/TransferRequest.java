453a8f80ba2d76ac5d1582e620181feb2285c188
lxcalbxy <ycxsbeats@yandex.ru>
2026-05-10 19:46:09 +0300

    feat: add TransferRequest DTO

--- src/main/java/lv/v3nom/application/dto/requests/TransferRequest.java
+++ src/main/java/lv/v3nom/application/dto/requests/TransferRequest.java
@@ -1,5 +1,31 @@
-package lv.v3nom.application.dto;
+package lv.v3nom.application.dto.requests;
+
+import java.math.BigDecimal;
 
 public class TransferRequest {
-    // TODO
+    private String currentSessionToken;
+    private String idempotencyKey;
+    private String sourceAccountId;
+    private String targetAccountId;
+    private BigDecimal amount;
+
+    public TransferRequest() {}
+    public TransferRequest(String currentSessionToken,
+                           String idempotencyKey,
+                           String sourceAccountId,
+                           String targetAccountId,
+                           BigDecimal amount) {
+
+        this.currentSessionToken = currentSessionToken;
+        this.idempotencyKey = idempotencyKey;
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.amount = amount;
+    }
+
+    public String getCurrentSessionToken() { return currentSessionToken; }
+    public String getIdempotencyKey() { return idempotencyKey; }
+    public String getSourceAccountId() { return sourceAccountId; }
+    public String getTargetAccountId() { return targetAccountId; }
+    public BigDecimal getAmount() { return amount; }
 }
